9e97871f2633c788c83497456f020d4d15299264
tkb0004413 <writer159.new@gmail.com>
2022-08-30 02:19:29 +0000

    material ui 配置

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Noto Sans TC'],
+  },
+  palette: {
+    primary: {
+      main: '#1a0b41',
+    },
+    secondary: {
+      main: '#c59a50',
+    },
+  },
+})
+
+export default theme
