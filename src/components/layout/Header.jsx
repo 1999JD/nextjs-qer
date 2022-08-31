3b6110279a5cc9b24c8c99c25a7cb1e58dc17537
tkb0004413 <writer159.new@gmail.com>
2022-08-31 13:39:59 +0000

    drawer 更換icon

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,14 +3,19 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
+import PaymentIcon from '@mui/icons-material/Payment';
 import IconButton from '@mui/material/IconButton';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ListAltIcon from '@mui/icons-material/ListAlt';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import RoomServiceIcon from '@mui/icons-material/RoomService';
 import ListItemText from '@mui/material/ListItemText';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
+import PolicyIcon from '@mui/icons-material/Policy';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -24,7 +29,8 @@ function ResponsiveDrawer(props) {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
+  const Icons = [<PaymentIcon />, <RoomServiceIcon />, <ListAltIcon />, <AccountCircleIcon />]
+  const Layer2Icons = [<MailIcon />, <PolicyIcon />]
   const drawer = (
     <div>
       <Toolbar />
@@ -34,7 +40,7 @@ function ResponsiveDrawer(props) {
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {Icons[index]}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
@@ -47,7 +53,7 @@ function ResponsiveDrawer(props) {
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {Layer2Icons[index]}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
